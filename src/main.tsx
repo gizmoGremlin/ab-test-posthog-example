f1a9697ecd9586ffc2e2ecd92690511d66db07b8
gizmoGremlin <nick.gilliosipescu@gmail.com>
2025-03-04 09:56:07 -0400

    fix linting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { Suspense, StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import { PostHogProvider } from 'posthog-js/react';
+import { HelmetProvider } from 'react-helmet-async';
 
 import App from './app';
+
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
 };
